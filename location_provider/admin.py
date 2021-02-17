d7501b9794df1df06987d4cb7667a2f22dee37f7
Christian Medlin <ChristianLMedlin@gmail.com>
2021-02-17 16:06:41 -0500

    Added LocationProvider to Admin

--- location_provider/admin.py
+++ location_provider/admin.py
@@ -4,3 +4,5 @@ from .models import LocationProvider
 
 class LocationProviderAdmin(admin.ModelAdmin):
     list_display = [field.name for field in LocationProvider._meta.fields]
+
+admin.site.register(LocationProvider, LocationProviderAdmin)
