189f48c193202ec4b716a8f70aad392da65a2fda
Christian Medlin <ChristianLMedlin@gmail.com>
2021-02-17 20:48:46 -0500

    Created list method for LocationProviderViewSet. list method can filter latitude and longitude query_params

--- location_provider/views.py
+++ location_provider/views.py
@@ -5,10 +5,25 @@ from rest_framework.response import Response
 from .serializers import LocationProviderSerializer
 from .models import LocationProvider
 
+
 class LocationProviderViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         queryset = LocationProvider.objects.all()
         location_data = get_object_or_404(queryset, pk=pk)
         serializer = LocationProviderSerializer(location_data)
+
+        return Response(serializer.data)
+
+    def list(self, request):
+        queryset = LocationProvider.objects.all()
+        #serializer = LocationProviderSerializer(queryset, many=True)
+        query = request.query_params
+        if "bottomLat" in query and "topLat" in query:
+            queryset = queryset.filter(
+                    latitude__range=(query["bottomLat"], query["topLat"]))
+        if "bottomLong" in query and "topLong" in query:
+            queryset = queryset.filter(
+                    longitude__range=(query["bottomLong"], query["topLong"]))
+        serializer = LocationProviderSerializer(queryset, many=True)
 
         return Response(serializer.data)
