ae46e6919b05c4858ead8c55bbef47f6cfafdf15
Christian Medlin <ChristianLMedlin@gmail.com>
2021-02-19 13:03:24 -0500

    Re-added STATIC_URL and STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -113,6 +113,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # CORS Settings
 
 CORS_ALLOW_ALL_ORIGINS = True
